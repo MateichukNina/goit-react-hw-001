6c73e3579b23108c65e29329aae0bca6bb2cae18
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2023-08-04 22:43:24 +0300

    upload again

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+import users from "./users.json";
+import data from "./data.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
+import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
+import FriendList from "./components/FriendList";
+import TransactionHistory from "./components/TransactionHistory";
+
+import "./App.css";
+
+function App() {
+  return (
+
+    <div>
+      <Profile
+        username={users.username}
+        tag={users.tag}
+        location={users.location}
+        avatar={users.avatar}
+        stats={users.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
+
+export default App;
